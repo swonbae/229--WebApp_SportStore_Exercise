19690b6e639ccc431b2ad593ed7541ad82f91ccd
swonb <swonbinca@gmail.com>
2019-08-30 16:26:35 -0400

    Initial Commit

---
+++ SportsStoreEx/Models/EFOrderRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStoreEx.Models
+{
+    public class EFOrderRepository : IOrderRepository
+    {
+        private ApplicationDbContext context;
+
+        public EFOrderRepository(ApplicationDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        // hard to understand this part below
+        public IQueryable<Order> Orders => context.Orders
+                            .Include(o => o.Lines)
+                            .ThenInclude(l => l.Product);
+
+        public void SaveOrder(Order order)
+        {
+            context.AttachRange(order.Lines.Select(l => l.Product));
+            if (order.OrderID == 0)
+            {
+                context.Orders.Add(order);
+            }
+            context.SaveChanges();
+        }
+    }
+}
